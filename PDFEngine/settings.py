d3d3b9e6a90ea5d01665cd66431bbe4059814804
Abhijeet Shahakar <abhijeet.shaha.kar.1@gmail.com>
2021-05-20 00:06:35 +0530

    Static_root removed from settings.py.

--- PDFEngine/settings.py
+++ PDFEngine/settings.py
@@ -131,7 +131,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
